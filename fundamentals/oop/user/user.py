ff391a06fd2681634c21e4960e98617160e8f165
s.ahmed <shyaahmed06@gmail.com>
2022-01-11 18:27:01 -0500

    Added 'make_deposit'

--- fundamentals/oop/user/user.py
+++ fundamentals/oop/user/user.py
@@ -3,8 +3,12 @@ class User:
         self.user_name = user_data["User"]
         self.account_balance = user_data["Balance"]
 
-    def make_withdrawl(self,amount):    
+    def make_deposit(self,amount):    
         self.account_balance += amount
+        return self.account_balance
+
+    def make_withdrawl(self,amount):    
+        self.account_balance -= amount
         return self.account_balance
 
 
@@ -12,10 +16,9 @@ class User:
         print(f"User : {self.user_name}, Balance : {self.account_balance}")
 
     def transfer_money(self,other_user,amount):
-        if(self.account_balance > 0):
+        if(self.account_balance > 0 and amount <= self.account_balance):
             self.account_balance -= amount
             second_user.account_balance += amount
-        return other_user
 
 
 first_user_data = {
@@ -36,20 +39,20 @@ third_user_data = {
 }
 third_user = User(third_user_data)
 
-first_user.make_withdrawl(200)
-first_user.make_withdrawl(300)
-first_user.make_withdrawl(600)
-first_user.make_withdrawl(-450)
+first_user.make_deposit(200)
+first_user.make_deposit(300)
+first_user.make_deposit(600)
+first_user.make_withdrawl(450)
 
-second_user.make_withdrawl(400)
-second_user.make_withdrawl(100)
-second_user.make_withdrawl(-800)
-second_user.make_withdrawl(-220)
+second_user.make_deposit(400)
+second_user.make_deposit(100)
+second_user.make_withdrawl(800)
+second_user.make_withdrawl(220)
 
-third_user.make_withdrawl(-1600)
-third_user.make_withdrawl(-930)
-third_user.make_withdrawl(-489)
-third_user.make_withdrawl(370)
+third_user.make_withdrawl(1600)
+third_user.make_withdrawl(930)
+third_user.make_withdrawl(489)
+third_user.make_deposit(370)
 
 print("\n===========================")
 first_user.display_user_balance()
@@ -57,7 +60,7 @@ second_user.display_user_balance()
 third_user.display_user_balance()
 print("===========================\n")
 
-first_user.transfer_money(second_user.account_balance, 200)
+first_user.transfer_money(second_user.account_balance, 400) # I changed it a little. If you go above the amount of your balance, it will not transfer!!
 first_user.display_user_balance()
 second_user.display_user_balance()
 print("===========================\n")
