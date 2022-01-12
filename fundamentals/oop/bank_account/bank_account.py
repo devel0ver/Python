ca41199b7b52dd690f77b471bc06605c98555648
s.ahmed <shyaahmed06@gmail.com>
2022-01-11 21:11:55 -0500

    commit

--- fundamentals/oop/bank_account/bank_account.py
+++ fundamentals/oop/bank_account/bank_account.py
@@ -16,29 +16,34 @@ class BankAccount:
         if (self.balance - amount) >= 0 :
             self.balance -= amount
         else:
-            print("Insufficient Funds")
+            print("Insufficient Funds: Charging a $5 fee")
+            self.balance -= 5
         return self
 
     def display_account_info(self):
         print(f"Balance : {self.balance}")
 
     def yeild_interest(self):
-        self.balance = (self.balance + self.balance) * self.int_rate
+        if self.balance > 0:
+            self.balance += (self.balance * self.int_rate) 
         return self
 
     @classmethod
     def total_acc(cls):
         print(f"\nThere are {cls.num_of_acc} accounts")
+    
+    @classmethod
+    def print_acc_info(cls):
+        for account in cls.acc_info:
+            account.display_account_info()
 
-first_acc = BankAccount(0.30, 0)
-second_acc = BankAccount(0.30, 0)
+savings = BankAccount(0.05, 1000)
+checking = BankAccount(0.02, 5000)
 BankAccount.total_acc()
 print("====================")
-first_acc.deposit(950).deposit(950).deposit(835).withdraw(1900).yeild_interest()
-first_acc.display_account_info()
-print("================")
-second_acc.deposit(2000).deposit(1450).withdraw(833).withdraw(500).withdraw(350).withdraw(1200).yeild_interest()
-second_acc.display_account_info()
+savings.deposit(10).deposit(50).deposit(150).withdraw(600).yeild_interest().display_account_info()
+checking.deposit(100).deposit(200).withdraw(200).withdraw(30).withdraw(350).withdraw(1200).yeild_interest().display_account_info()
+BankAccount.print_acc_info()
 print("================")
 print(BankAccount.acc_info)
 print("======================================\t\t============================================")
