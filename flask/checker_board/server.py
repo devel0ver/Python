6916efbcfddc8eb01cfee95d0010f8b61bcb1515
s.ahmed <shyaahmed06@gmail.com>
2022-01-14 15:47:02 -0500

    Commit

--- flask/checker_board/server.py
+++ flask/checker_board/server.py
@@ -3,15 +3,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def play():
-    return render_template("index.html")
+    return render_template("index.html", rows=8, col=8)
 
-@app.route('/play/<int:num>')
-def count(num):
-    return render_template("index.html", num = num)
+@app.route('/<int:col>/<string:color>/<string:color2>')
+def count(col, color, color2):
+    return render_template("index.html", col=col, rows=8, color = color, color2=color2)
 
-@app.route('/play/<int:num>/<string:color>')
-def colors(num, color):
-    return render_template("index.html", num = num, color = color)
+@app.route('/<int:rows>/<int:col>')
+def colors(rows, col):
+    return render_template("index.html", rows = rows, col = col)
 
 
 if __name__ == "__main__":
