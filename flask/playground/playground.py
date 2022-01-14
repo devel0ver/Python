4af4b6badfa787197465b1ee685991539408657e
s.ahmed <shyaahmed06@gmail.com>
2022-01-13 20:57:45 -0500

    First Commit

---
+++ flask/playground/playground.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/play')
+def play():
+    return render_template("index.html")
+
+@app.route('/play/<int:num>')
+def count(num):
+    return render_template("times.html", num = num)
+
+@app.route('/play/<int:num>/<string:color>')
+def colors(num, color):
+    return render_template("times.html", num = num, color = color)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
